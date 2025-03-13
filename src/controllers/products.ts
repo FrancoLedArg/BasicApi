da484ae717f7c37de414ab70953693a27d819b4c
FrancoLedesma <francoledesma100@gmail.com>
2025-03-13 19:56:17 -0300

    Added query params to general findAll request

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -5,20 +5,17 @@ import { findAll } from "@/services/products";
 
 export const getProducts = async (req: Request, res: Response) => {
   try {
-    /*
-    const { limit, offset } = req.query;
+    const limit = req.query.limit ? parseInt(req.query.limit as string) : 10;
+    const offset = req.query.offset ? parseInt(req.query.offset as string) : 0;
 
-    if (limit != undefined && offset != undefined) {
-      res.json({
-        success: true,
-        message: "I'm an endpoint",
-        limit,
-        offset,
+    if (isNaN(limit) || isNaN(offset) || limit < 1 || offset < 0) {
+      res.status(400).json({
+        success: false,
+        message: "Invalid limit or offset",
       });
     }
-      */
 
-    const products = await findAll();
+    const products = await findAll(limit, offset);
 
     res.status(200).json({
       success: true,
--- src/services/products.ts
+++ src/services/products.ts
@@ -1,7 +1,11 @@
 import { db } from "@/lib/db";
 
-export const findAll = async () => {
-  const products = await db.query.products.findMany();
+export const findAll = async (limit: number, offset: number) => {
+  const products = await db.query.products.findMany({
+    limit,
+    offset,
+  });
+
   if (!products) {
     throw new Error("Database Error");
   }
